d1a3e1ec5538a4595b0da0876a5df62a96f62c5f
Yudensei <yudensei.140@hotmail.com>
2025-05-25 03:24:32 -0300

    Finish menu section

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import * as temp from "./template.js";
+import { loadMenu } from "./menu.js";
+
+
 import seam from "./seam.png";
 import seamBg from "./seam-bg.png";
 import susie from "./susie_sweatdrop.png";
@@ -6,27 +9,48 @@ import "./style.css";
 
 const content = document.querySelector("#content");
 
-temp.char.src = seam;
-temp.shopBg.src = seamBg;
+const navigation = document.querySelector("nav");
 
-temp.bg.appendChild(temp.char);
-temp.bg.appendChild(temp.shopBg);
+loadHome();
 
-content.appendChild(temp.bg);
+navigation.addEventListener("click", (e) => {
+    if (!e.target.id) return;
+    unload();
+    
+    if (e.target.id === "home") loadHome();
+    else if (e.target.id === "menu") loadMenu();
+})
+
+function loadHome() {
+  temp.char.src = seam;
+  temp.shopBg.src = seamBg;
+
+  temp.bg.appendChild(temp.char);
+  temp.bg.appendChild(temp.shopBg);
 
 
-temp.greet.innerHTML = `<p>* Hee hee... Welcome, travellers.</p>
-                        <p>* Take your time... Ain't like it's better spent.</p>`
-
-content.appendChild(temp.greet);
+  temp.greet.innerHTML = `<p>* Hee hee... Welcome, travellers.</p>
+                        <p>* Take your time... Ain't like it's better spent.</p>`;
 
 
-const diagBox = temp.dialogue.cloneNode();
-diagBox.innerHTML = `<p>* Greetings, the name's Seam, pronounced "Shawn", and this is the most famous restaurant in all of Dark Worlds.</p>
+  const diagBox = temp.dialogue.cloneNode();
+  diagBox.innerHTML = `<p>* Greetings, the name's Seam, pronounced "Shawn", and this is the most famous restaurant in all of Dark Worlds.</p>
                      <p>* We have a lot of stuff here... not that any of it matters.</p>
                      <p>* Look around, and you may find something of your interest... or even more...</p>
                      <p>* We also give 50% discounts in case of any purple monster attack.</p>
-                     <p class="comment"><img src="${susie}" class="comment-portrait">* Are they talking about me?</p>`
+                     <p class="comment"><img src="${susie}" class="comment-portrait">* Are they talking about me?</p>`;
 
-temp.main.appendChild(diagBox)
-content.appendChild(temp.main)
+  temp.main.appendChild(diagBox);
+  
+  
+  content.appendChild(temp.bg);
+  content.appendChild(temp.greet);
+  content.appendChild(temp.main);
+}
+
+function unload() {
+    content.textContent = "";
+    for (let elem of Object.values(temp)) {
+        elem.textContent = "";
+    }
+}
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,59 @@
-function hi() {
-    console.log("Hi")
+import * as temp from "./template.js";
+import swatch from "./swatch.png";
+import swatchBg from "./swatch-bg.png";
+
+
+
+const content = document.querySelector("#content");
+
+function loadMenu() {
+    temp.char.src = swatch;
+    temp.shopBg.src = swatchBg;
+  
+    temp.bg.appendChild(temp.char);
+    temp.bg.appendChild(temp.shopBg);
+  
+  
+    temp.greet.innerHTML = `<p>* Welcome to the Color Cafe. Let us warm your day.</p>
+                          <p>* Don't be blue. We're here for you.</p>`;
+  
+  
+    const diagBox = temp.dialogue.cloneNode();
+    diagBox.innerHTML = `<p>* I am Swatch, Our Lady Grace's head butler.</p>
+                       <p>* This restaurant is funded by our dear Queen, so me and my staff are still attending to her needs.</p>
+                       <p>* Feel free to look around and pick what you like, it'll be our delight to serve you.</p>`;
+  
+    temp.main.appendChild(diagBox);
+
+    const item1 =  temp.dialogue.cloneNode();
+    item1.innerHTML = "<h1>Dark Candy</h1> <p>A red-and-black star that tastes like marshmallows.</p>";
+
+    const item2 = temp.dialogue.cloneNode();
+    item2.innerHTML = "<h1>Darkbuger</h1> <p>A mysterious black burger made of... Hey, this is just burnt!</p>";
+
+    const item3 = temp.dialogue.cloneNode();
+    item3.innerHTML = "<h1>CD Bagel</h1> <p>A bagel with a reflective inside. Makes music with each bite.</p>";
+
+    const item4 = temp.dialogue.cloneNode();
+    item4.innerHTML = "<h1>DD Burger</h1> <p>It's the Double-Dark-Burger. It'll take two bites to finish!</p>";
+
+    const item5 = temp.dialogue.cloneNode();
+    item5.innerHTML = "<h1>Spaghetti Code</h1> <p>Spaghetti woven by master coders, made of macarons and ribbons.</p>";
+
+    const item6 = temp.dialogue.cloneNode();
+    item6.innerHTML = "<h1>Java Cookie</h1> <p>A coffee-and-chocolate flavored cookie. Words spark out when you bite it.</p>";
+
+    
+    temp.main.appendChild(item1);
+    temp.main.appendChild(item2);
+    temp.main.appendChild(item3);
+    temp.main.appendChild(item4);
+    temp.main.appendChild(item5);
+    temp.main.appendChild(item6);
+
+    content.appendChild(temp.bg);
+    content.appendChild(temp.greet);
+    content.appendChild(temp.main);
 }
 
-export { hi };
+export { loadMenu }
